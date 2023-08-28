a586144dda6e8057385d6f5a2f3d5dc8721325b2
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-28 17:22:42 +0800

    西元2023年08月28日 (週一) 17時22分42秒 CST

--- config-list.js
+++ config-list.js
@@ -330,6 +330,12 @@ let feedList = [
     homepageURL: 'https://www.youtube.com/channel/UCoZ9EFEofFgyRCdh5tPM2yw',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
+  { // <outline type="rss" text="游戲指南針++$0414-1925$" title="游戲指南針++$0414-1925$" xmlUrl="http://pulipuli.myqnapcloud.com/304/fc/$0414-1925$/https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCVjXB6QUf_aZ_FninmssCOg" htmlUrl="https://www.youtube.com/channel/UCVjXB6QUf_aZ_FninmssCOg"/>
+    title: '游戲指南針',
+    feedID: 'YouXiZhiNanZhen',
+    homepageURL: 'https://www.youtube.com/channel/UCVjXB6QUf_aZ_FninmssCOg',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
 ]
 
 // feedList = [
